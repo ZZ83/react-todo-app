a6b13fbbbe1c147e9ab3fc266365b09e7ba8f250
ZZ83 <13670661+ZZ83@users.noreply.github.com>
2023-10-12 20:47:17 -0700

    Stylelint Wrapper

--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 
-const StyledWrapper = styled.div`
-    padding: 0 24px;
+const $Wrapper = styled.div`
     max-width: 588px;
+    padding: 0 24px;
     margin: 0 auto;
 `;
 
 function Wrapper({ children }) {
-    return <StyledWrapper>{children}</StyledWrapper>;
+    return <$Wrapper>{children}</$Wrapper>;
 }
 
 export default Wrapper;
